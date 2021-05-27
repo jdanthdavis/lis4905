2b773d8b453885a2811203afde13199776bcbd99
sa17d <sa17d@my.fsu.edu>
2021-05-27 14:11:36 -0400

    week2 work

---
+++ projects/sa17d/week2/tip_calculator.py
@@ -0,0 +1,33 @@
+# program reqs
+print("tip calculator")
+
+# dev: Seb Angel-Riano
+# course: LIS4905
+# sum2021
+
+print("\nProgram Requirements: \n"
+ + "1. Must use float data type for user input (except \"party number\").\n"
+ + "2. Must round calculations to two decimal places.\n"
+ + "3. Must format currency with dollar sign and two decimal places.")
+
+print("\nUser Input:")
+meal_cost = float(input( "Cost of meal: "))
+tax_percent = float(input( "Tax percent: "))
+tip_percent = float(input( "Tip percent: "))
+people_num = int(input( "Party number: "))
+
+#Calculate tax, tip and total amount
+tax_amount = round(meal_cost * (tax_percent / 100), 2) # convert to percentage
+due_amount = round(meal_cost + tax_amount, 2)
+tip_amount = round((due_amount) * (tip_percent / 100), 2) #percentage of tax
+total = round(meal_cost + tax_amount + tip_amount, 2)
+split = round(total / people_num, 2)
+
+#display results
+print("\nProgram Output:")
+print("Subtotal:\t", "${0:,.2f}".format(meal_cost))
+print("Tax:\t\t", "${0:,.2f}".format(tax_amount))
+print("Amount Due:\t", "${0:,.2f}".format(due_amount))
+print("Gratituy:\t", "${0:,.2f}".format(tip_amount))
+print("Total:\t\t", "${0:,.2f}".format(total))
+print("Split " + "(" + str(people_num) + "):\t", "${0:,.2f}".format(split))
