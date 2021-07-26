85b4bb8ec6ba2286a9ee595d054103000bf6dd40
jd19z <jd19z@my.fsu.edu>
2021-07-26 17:39:45 -0400

    Final Push

--- projects/jd19z/Week10/docs/opencv.py
+++ projects/jd19z/Week10/docs/opencv.py
@@ -1,22 +1,22 @@
 import cv2 as cv
 import numpy as np
-import os as os
+import matplotlib.pyplot as plt
 
-# def get_requirements():
-#     print("\nOpenCV - Computer Vision Library (Install this version: opencv-contrib-python)\n")
-#     print("\nProgram Requirements:\n"
-#         + "1. Image Processing and Manipulation.\n"
-#         + "2. Import necessary libraries.\n"
-#         + "3. Research how to install any missing packages, if necessary.\n"
-#         + "4. Create at least three functions that are called by the program:\n"
-#         + "     a. main(): calls at least two other functions.\n"
-#         + "     b. get_requirements(): displays the program requirements\n"
-#         + "     c. opencv(): displays the following data.\n"
-#         + "5. Use provided media files.\n"
-#         + "6.  When running program:\n"
-#         + "     a. Document any issues.\n"
-#         + "     b. Document solutions attempted.\n")
-#     print("***Research how to make it work using Jupyter Notebook***")
+def get_requirements():
+    print("\nOpenCV - Computer Vision Library (Install this version: opencv-contrib-python)\n")
+    print("\nProgram Requirements:\n"
+        + "1. Image Processing and Manipulation.\n"
+        + "2. Import necessary libraries.\n"
+        + "3. Research how to install any missing packages, if necessary.\n"
+        + "4. Create at least three functions that are called by the program:\n"
+        + "     a. main(): calls at least two other functions.\n"
+        + "     b. get_requirements(): displays the program requirements\n"
+        + "     c. opencv(): displays the following data.\n"
+        + "5. Use provided media files.\n"
+        + "6.  When running program:\n"
+        + "     a. Document any issues.\n"
+        + "     b. Document solutions attempted.\n")
+    print("***Research how to make it work using Jupyter Notebook***")
 
 def opencv():
 # read in images (return images as matrix of pixels)
@@ -94,44 +94,61 @@ def opencv():
     # np.zeros: Returns new array of given shape and type, filled with zeros.
     # a. display blank image
     blank_image = np.zeros((height, width, num_colors), dtype='uint8')
-    cv.imshow('Blank', blank_image)
+    plt.title('Blank', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # b. color image (BGR values)
     blank_image[:] = 255, 0, 0
-    cv.imshow('Blue', blank_image)
+    plt.title('Blue', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # c. color only range of pixels (red square in blue image)
     blank_image[100:200, 300:400] = 0, 0, 255
-    cv.imshow('Blue w/ Red Square', blank_image)
+    plt.title('Blue w/ Red Square', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # d. draw rectangle (img, start_point, end_point, color, thickness)
     cv.rectangle(blank_image, (0, 0), (200, 200), (0, 255, 0), thickness = 2)
-    cv.imshow('Rectangle', blank_image)
+    plt.title('Rectangle', blank_image)
+    plt.show()
 
     # e. draw rectangle w/ filled color (same as thickness = -1)
     cv.rectangle(blank_image, (0, 0), (200, 200), (0, 255, 0), thickness = cv.FILLED)
-    cv.imshow('Rectangle Filled', blank_image)
+    plt.title('Rectangle Filled', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # here, using ratio of height and width
     # Note: integer division operator // (only keeps whole numbers)
     cv.rectangle(blank_image, (0, 0), (blank_image.shape[1] // 4, blank_image.shape[0] // 4),
                     (255, 255, 255), thickness = -1)
-    cv.imshow('Rectangle Filled Ratio', blank_image)
+    plt.title('Rectangle Filled Ratio', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # f. draw a circle (img, center, radius, color, thickness)
     cv.circle(blank_image, (blank_image.shape[1] // 2, blank_image.shape[0] // 2), 30,
                 (0, 0, 0), thickness = 3)
-    cv.imshow('Circle', blank_image)
+    plt.title('Circle', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # g. draw line (img, start_point, end_point, color, thickness)
     cv.line(blank_image, (blank_image.shape[1], blank_image.shape[0]), (blank_image.shape[1] // 2, blank_image.shape[0] // 2),
                 (255, 255, 255), thickness = 1)
-    cv.imshow('Line', blank_image)
+    plt.title('Line', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
 
     # h. write text (img, text, origin, fontFace, fontScale, color, thickness)
     cv.putText(blank_image, 'Hello World!',
                 (blank_image.shape[1] // 2, blank_image.shape[0] // 2), cv.FONT_HERSHEY_SCRIPT_COMPLEX, 1.5, (0, 255, 0), thickness = 2)
-    cv.imshow('Text', blank_image)
+    plt.title('Text', blank_image)
+    plt.imshow(blank_image)
+    plt.show()
     cv.waitKey(0)
 
 # resize/rescale images
@@ -150,9 +167,28 @@ def changeRes(width, height):
     capture.set(3, width)
     capture.set(4, height)
 
+# def notebook():
+#     img1 = cv.imread('photos/dog.jpg')
+#     img2 = cv.imread('photos/dog.jpg')
+
+#     NUM_ROWS = 1
+#     IMGs_IN_ROW = 2
+#     f, ax = plt.subplots(NUM_ROWS, IMGs_IN_ROW, figsize=(16,6))
+
+#     ax[0].imshow(img1)
+#     # ax[1].imshow(img2)
+#     ax[1].imshow(cv.cvtColor(img2, cv.COLOR_BGR2RGB))
+
+#     ax[0].set_title('Blue Dog')
+#     ax[1].set_title('Happy Dog -- or, maybe not!')
+
+#     title = ('Side-by-Side Image Views - BETTER!')
+#     f.suptitle(title, fontsize=16)
+#     plt.tight_layout()
+#     plt.show()
+
 def main():
-    # get_requirements()
-    opencv()
+    notebook();
 
 if __name__ == "__main__":
     main()
